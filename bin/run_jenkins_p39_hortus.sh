596a82ecb91b98f1054a16af5c40af57334bc1bc
team@agriscope.fr <you@example.com>
2022-01-12 16:45:24 +0100

    clean comments

--- bin/run_jenkins_p39_hortus.sh
+++ bin/run_jenkins_p39_hortus.sh
@@ -5,9 +5,6 @@
 source /home/tomcat/pyenv/venv_39_django3/bin/activate # Activation de l'environnement virtuel
 
 
-#pip install -r ./djcan/requirements.txt # Installation des dépendances pour l'application
-#python -m unittest discover project_directory "*_test.py"
-
 
 nosetests  --with-xunit    ` find agstream/tests/ -name "*test*.py" `  --with-coverage --cover-package=agstream
 coverage xml
